e7aae4dcd2dc3bafae76909ff6349edfcb1a1900
Yorn Meun <yornmeun@gmail.com>
2026-06-26 22:48:19 +0700

    fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable, HasRoles;
 
@@ -61,5 +63,10 @@ class User extends Authenticatable
     public function quotations()
     {
         return $this->hasMany(Quotation::class);
+    }
+
+      public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->hasRole('super_admin');
     }
 }
